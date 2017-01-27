b3c5141a0411074b89ce92c1ad8c09677c090d05
SvitlanaKo <SvitlanaKo@users.noreply.github.com>
2017-01-27 19:43:08 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -37,8 +37,8 @@ from pygments.lexers.web import PhpLexer
 # 'sensio.sphinx.configurationblock',
 
 # set url for API links
-api_url = 'http://phpdoc.orocrm.com/platform/%s'
-api_url_pattern = 'http://phpdoc.orocrm.com/platform/{namespace}namespaces{/namespace}{class}classes{/class}{method}classes{/method}/%(namespace)s{class}.%(class)s{/class}{method}.%(class)s{/method}.html{method}#method_%(method)s{/method}'
+#api_url = 'http://phpdoc.orocrm.com/platform/%s'
+#api_url_pattern = 'http://phpdoc.orocrm.com/platform/{namespace}namespaces{/namespace}{class}classes{/class}{method}classes{/method}/%(namespace)s{class}.%(class)s{/class}{method}.%(class)s{/method}.html{method}#method_%(method)s{/method}'
 namespace_separator = '.'
 
 # Add any paths that contain templates here, relative to this directory.
